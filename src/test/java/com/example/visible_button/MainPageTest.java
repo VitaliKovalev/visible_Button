bc6b52e0a05919f8478ecc2d992c799746490973
VitaliKovalev <zubavetal@gmail.com>
2023-05-09 01:40:12 +0300

    Initial commit

---
+++ src/test/java/com/example/visible_button/MainPageTest.java
@@ -0,0 +1,44 @@
+package com.example.visible_button;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void visible() {
+        driver.get("https://demoqa.com/dynamic-properties");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        WebElement hideButton = driver.findElement(By.cssSelector("#visibleAfter"));
+        wait.until(ExpectedConditions.visibilityOf(hideButton));
+        assertTrue(hideButton.isEnabled(), "Кнопка не стала видимой");
+    }
+}
